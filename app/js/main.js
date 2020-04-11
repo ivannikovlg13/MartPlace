bbca37ec33fdf05be2a9169637f94fa3686a32ff
ivannikovlg13 <livannikov55@gmail.com>
2020-04-11 18:33:45 +0300

    added slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function () {
 
-  
+  $(".rate-star").rateYo({
+    rating: 4.5,
+    starWidth: "17px",
+    readOnly: true
+  });
+
+  $('.weekly__slider-inner').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__prev" src="images/back.svg" alt=""></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__next" src="images/right.svg" alt=""></img>',
+  });
 
 });
+
+

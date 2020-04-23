6fc5bf0cb2af2d976239f597bee2d750f2c0c980
ivannikovlg13 <livannikov55@gmail.com>
2020-04-23 21:54:28 +0300

    added page category

--- app/js/main.js
+++ app/js/main.js
@@ -30,6 +30,44 @@ $(function () {
     nextArrow: '<img class="slider-arrows slider-arrows__next" src="images/right.svg" alt=""></img>',
   });
 
+  $('.linearicons-list4').on('click', function () {
+    $('.categories__item').addClass('list');
+    $('.linearicons-list4').addClass('active');
+    $('.linearicons-border-outer').removeClass('active');
+  });
+  $('.linearicons-border-outer').on('click', function () {
+    $('.categories__item').removeClass('list');
+    $('.linearicons-border-outer').addClass('active');
+    $('.linearicons-list4').removeClass('active');
+  });  
+
+  $('.categories__aside-title-link').on('click',function(){
+    $('.category__list-link').addClass('active').slideToggle();
+  });
+
+  $('.categories__aside-title-radio').on('click', function() {
+    $('.category__list-radio').addClass('active').slideToggle();
+  });
+  
+  $('.categories__aside-title-range').on('click', function(){
+    $('.category__list-range').addClass('active').slideToggle();
+  });
+
+
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 320,
+    from: 0,
+    to: 300,
+    prefix: "$"
+  });
+
+
+
+  $('input, select').styler();
+
 
   var mixer = mixitup('.release__inner-box');
 
